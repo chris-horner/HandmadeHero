7eed61fddaaab46705d327561e2628589f58e1ad
Christopher Horner <horner.cd@gmail.com>
2016-04-17 18:51:54 +1000

    002 Opening a Win32 Window

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 mkdir build
 pushd build
-cl -Zi ..\win32_handmade.cpp user32.lib
+cl -Zi ..\win32_handmade.cpp user32.lib gdi32.lib
 popd
 
--- win32_handmade.cpp
+++ win32_handmade.cpp
@@ -1,13 +1,113 @@
 #include<windows.h>
 
-int CALLBACK WinMain(
-  HINSTANCE hInstance,
-  HINSTANCE hPrevInstance,
-  LPSTR lpCmdLine,
-  int nCmdShow
-)
+LRESULT CALLBACK MainWindowCallback(
+    HWND Window,
+    UINT Message,
+    WPARAM WParam,
+    LPARAM LParam)
 {
-  MessageBoxA(0, "This is Handmade Hero.", "Handmade Hero", MB_OK|MB_ICONINFORMATION);
+  LRESULT Result = 0;
+
+  switch(Message)
+  {
+    case WM_SIZE:
+    {
+      OutputDebugStringA("WM_SIZE\n");
+    } break;
+
+    case WM_DESTROY:
+    {
+      OutputDebugStringA("WM_DESTROY\n");
+    } break;
+
+    case WM_CLOSE:
+    {
+      OutputDebugStringA("WM_CLOSE\n");
+    } break;
+
+    case WM_ACTIVATEAPP:
+    {
+      OutputDebugStringA("WM_ACTIVATEAPP\n");
+    } break;
+
+    case WM_PAINT:
+    {
+      PAINTSTRUCT Paint;
+      HDC DeviceContext = BeginPaint(Window, &Paint);
+      int X = Paint.rcPaint.left;
+      int Y = Paint.rcPaint.top;
+      int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+      int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+      PatBlt(DeviceContext, X, Y, Width, Height, WHITENESS);
+      EndPaint(Window, &Paint);
+    } break;
+
+    default:
+    {
+      //OutputDebugStringA("default\n")
+      Result = DefWindowProc(Window, Message, WParam, LParam);
+    } break;
+  }
+
+  return(Result);
+}
+
+int CALLBACK WinMain(
+    HINSTANCE Instance,
+    HINSTANCE PrevInstance,
+    LPSTR CommandLine,
+    int ShowCode)
+{
+  WNDCLASS WindowClass = {};
+
+  WindowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
+  WindowClass.lpfnWndProc = MainWindowCallback;
+  WindowClass.hInstance = Instance;
+  WindowClass.lpszClassName = "HandmadeHeroWindowClass";
+
+  if (RegisterClass(&WindowClass))
+  {
+    HWND WindowHandle = CreateWindowEx(
+          0,
+          WindowClass.lpszClassName,
+          "Handmade Hero",
+          WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+          CW_USEDEFAULT,
+          CW_USEDEFAULT,
+          CW_USEDEFAULT,
+          CW_USEDEFAULT,
+          0,
+          0,
+          Instance,
+          0);
+
+    if (WindowHandle)
+    {
+      for (;;)
+      {
+        MSG Message;
+        BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+
+        if (MessageResult > 0)
+        {
+          TranslateMessage(&Message);
+          DispatchMessage(&Message);
+        }
+        else
+        {
+          break;
+        }
+      }
+    }
+    else
+    {
+      // TODO Logging
+    }
+  }
+  else
+  {
+    // TODO Logging
+  }
 
   return(0);
 }
