e1e88a759db82d8db53efd75130016786241f8a2
Christopher Horner <horner.cd@gmail.com>
2016-05-08 10:19:20 +1000

    Request absolute paths for build errors.

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 @echo off
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
 mkdir build
 pushd build
-cl -Zi ..\win32_handmade.cpp user32.lib gdi32.lib
+cl -Zi -FC ..\win32_handmade.cpp user32.lib gdi32.lib
 popd
 
