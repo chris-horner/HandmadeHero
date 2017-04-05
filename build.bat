5698554c43b4ddc06e7a183a0e8aa4a2967365a1
Christopher Horner <horner.cd@gmail.com>
2017-04-05 17:05:35 +1000

    Update build batch file for VS 2017.

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 IF NOT EXIST build mkdir build
 pushd build
 cl -DHANDMADE_WIN32=1 -Zi -FC ..\win32_handmade.cpp user32.lib gdi32.lib
