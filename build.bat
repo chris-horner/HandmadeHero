9246f61b579ca9b4e858810278694deeac205600
Christopher Horner <horner.cd@gmail.com>
2017-04-14 19:58:10 +1000

    Move source files into src directory.

--- build.bat
+++ build.bat
@@ -2,6 +2,6 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 IF NOT EXIST build mkdir build
 pushd build
-cl -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 -DHANDMADE_WIN32=1 -Zi -FC ..\win32_handmade.cpp user32.lib gdi32.lib
+cl -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 -DHANDMADE_WIN32=1 -Zi -FC ..\src\win32_handmade.cpp user32.lib gdi32.lib
 popd
 
